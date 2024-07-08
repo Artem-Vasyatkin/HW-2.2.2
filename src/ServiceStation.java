f2e08c0417cd828b81c62da6cbeef16e33f3cf49
Artem Vasyatkin <dustler98@gmail.com>
2024-07-08 10:47:20 +0300

    hw done

---
+++ src/ServiceStation.java
@@ -0,0 +1,8 @@
+public class ServiceStation{
+
+    public void check(Vehicle vehicle) {
+        vehicle.checkService();
+        System.out.printf("Обслуживается %s%n" , vehicle.getModelName());
+    }
+
+}
