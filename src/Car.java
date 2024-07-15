ccc5e04f96b11f770ba1025642cee12214361fad
Artem Vasyatkin <dustler98@gmail.com>
2024-07-15 11:13:16 +0300

    Created a pull request

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,16 @@
-public class Car {
+public class Car extends Vehicle{
 
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void checkService() {
+        updateTyre();
+        checkEngine();
+    }
+
+    public void checkEngine(){
+        System.out.println("Проверяем двигатель");
+    }
 }
--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,2 +1,7 @@
-public class ServiceStation{
+public class ServiceStation {
+
+    public void check(Vehicle vehicle) {
+        vehicle.checkService();
+        System.out.printf("Обслуживается %s%n" , vehicle.getModelName());
+    }
 }
